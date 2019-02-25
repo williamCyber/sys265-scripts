972697386f6f1fd353fb2bb55421b1a66ffdecf5
williamCyber <william.standen@mymail.champlain.edu>
2019-02-25 12:13:24 -0500

    Organized Directories

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,8 @@
+#SECURE-SSH.SH
+#AUTHOR WILLIAMCYBER
+#Creates a new ssh user called $1
+#Adds a public key to that users authorized keys file
+#Removes roots ability to ssh in
+
+echo "testing 123"
+
